357820a7110a69204cea6e13bc1f7178404402fb
vector <abdullah.harmonytx@gmail.com>
2026-06-30 08:40:47 -0500

    Fix AlertTriangle import formatting in BookmarksPage.tsx

--- ui/src/pages/BookmarksPage.tsx
+++ ui/src/pages/BookmarksPage.tsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState, useCallback } from 'react'
 import {
   Bookmark, Plus, X, Trash2, Loader, AlertCircle, Check,
   Info, Search, Edit3, Save, Link, Folder, ExternalLink
-, AlertTriangle} from 'lucide-react'
+, AlertTriangle
+} from 'lucide-react'
 import api from '../utils/api'
 
 interface BookmarkItem {
