59d68876758fa39483850058b3836236fa401ead
vector <abdullah.harmonytx@gmail.com>
2026-06-30 08:40:39 -0500

    Fix AlertTriangle import formatting in VideosPage.tsx

--- ui/src/pages/VideosPage.tsx
+++ ui/src/pages/VideosPage.tsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState, useCallback } from 'react'
 import {
   Video, Play, Search, Loader, AlertCircle, Check, Info,
   X, Clock, HardDrive, Film
-, AlertTriangle} from 'lucide-react'
+, AlertTriangle
+} from 'lucide-react'
 import api from '../utils/api'
 
 interface VideoFile {
